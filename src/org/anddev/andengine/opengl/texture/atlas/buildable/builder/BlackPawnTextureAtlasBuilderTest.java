6930a291fbe6ad4167b4e51fe182a50681b3b9ff
Nicolas Gramlich <ngramlich@zynga.com>
2011-11-22 14:33:44 +0100

    Updated BlackPawnTextureAtlasBuilderTest.

--- src/org/anddev/andengine/opengl/texture/atlas/buildable/builder/BlackPawnTextureAtlasBuilderTest.java
+++ src/org/anddev/andengine/opengl/texture/atlas/buildable/builder/BlackPawnTextureAtlasBuilderTest.java
@@ -3,7 +3,7 @@ package org.anddev.andengine.opengl.texture.atlas.buildable.builder;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
-import org.anddev.andengine.opengl.texture.atlas.buildable.builder.BlackPawnTextureBuilder.Node;
+import org.anddev.andengine.opengl.texture.atlas.buildable.builder.BlackPawnTextureAtlasBuilder.Node;
 import org.anddev.andengine.opengl.texture.atlas.source.ITextureAtlasSource;
 import org.anddev.andengine.util.modifier.IModifier.DeepCopyNotSupportedException;
 
@@ -11,13 +11,15 @@ import org.anddev.andengine.util.modifier.IModifier.DeepCopyNotSupportedExceptio
  * @author Nicolas Gramlich
  * @since 16:08:32 - 12.08.2010
  */
-public class BlackPawnTextureBuilderTest extends TestCase {
+public class BlackPawnTextureAtlasBuilderTest extends TestCase {
 	// ===========================================================
 	// Constants
 	// ===========================================================
 
 	private static final int SPACING_NONE = 0;
 	private static final int SPACING_ONE = 1;
+	private static final int PADDING_NONE = 0;
+	private static final int PADDING_ONE = 1;
 
 	// ===========================================================
 	// Fields
@@ -41,84 +43,84 @@ public class BlackPawnTextureBuilderTest extends TestCase {
 
 	public void testSingle() {
 		final Node node = new Node(0, 0, 10, 10);
-		final Node inserted = node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
+		final Node inserted = node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
 
 		this.assertNodeRect(inserted, 0, 0, 4, 2);
 	}
 
 	public void testDouble() {
 		final Node node = new Node(0, 0, 10, 10);
-		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
+		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
 
-		final Node inserted = node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
+		final Node inserted = node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
 
 		this.assertNodeRect(inserted, 4, 0, 4, 2);
 	}
 
 	public void testTriple() {
 		final Node node = new Node(0, 0, 10, 10);
-		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
-		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
+		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
+		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
 
-		final Node inserted = node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
+		final Node inserted = node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
 
 		this.assertNodeRect(inserted, 0, 2, 4, 2);
 	}
 
 	public void testQuadrupel() {
 		final Node node = new Node(0, 0, 10, 10);
-		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
-		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
-		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
+		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
+		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
+		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
 
-		final Node inserted = node.insert(new DummyTextureSource(2, 2), 10, 10, SPACING_NONE);
+		final Node inserted = node.insert(new DummyTextureSource(2, 2), 10, 10, SPACING_NONE, PADDING_NONE);
 
 		this.assertNodeRect(inserted, 8, 0, 2, 2);
 	}
 
 	public void testQuadrupel2() {
 		final Node node = new Node(0, 0, 10, 10);
-		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
-		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
-		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
+		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
+		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
+		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
 
-		final Node inserted = node.insert(new DummyTextureSource(2, 3), 10, 10, SPACING_NONE);
+		final Node inserted = node.insert(new DummyTextureSource(2, 3), 10, 10, SPACING_NONE, PADDING_NONE);
 
 		this.assertNodeRect(inserted, 0, 4, 2, 3);
 	}
 
 	public void testQuintupel() {
 		final Node node = new Node(0, 0, 10, 10);
-		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
-		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
-		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
-		node.insert(new DummyTextureSource(2, 3), 10, 10, SPACING_NONE);
+		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
+		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
+		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
+		node.insert(new DummyTextureSource(2, 3), 10, 10, SPACING_NONE, PADDING_NONE);
 
-		final Node inserted = node.insert(new DummyTextureSource(2, 3), 10, 10, SPACING_NONE);
+		final Node inserted = node.insert(new DummyTextureSource(2, 3), 10, 10, SPACING_NONE, PADDING_NONE);
 
 		this.assertNodeRect(inserted, 2, 4, 2, 3);
 	}
 
 	public void testQuintupel2() {
 		final Node node = new Node(0, 0, 10, 10);
-		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
-		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
-		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
-		node.insert(new DummyTextureSource(2, 3), 10, 10, SPACING_NONE);
+		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
+		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
+		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
+		node.insert(new DummyTextureSource(2, 3), 10, 10, SPACING_NONE, PADDING_NONE);
 
-		final Node inserted = node.insert(new DummyTextureSource(2, 8), 10, 10, SPACING_NONE);
+		final Node inserted = node.insert(new DummyTextureSource(2, 8), 10, 10, SPACING_NONE, PADDING_NONE);
 
 		this.assertNodeRect(inserted, 4, 2, 2, 8);
 	}
 
 	public void testQuintupel3() {
 		final Node node = new Node(0, 0, 10, 10);
-		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
-		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
-		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE);
-		node.insert(new DummyTextureSource(2, 3), 10, 10, SPACING_NONE);
+		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
+		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
+		node.insert(new DummyTextureSource(4, 2), 10, 10, SPACING_NONE, PADDING_NONE);
+		node.insert(new DummyTextureSource(2, 3), 10, 10, SPACING_NONE, PADDING_NONE);
 
-		final Node inserted = node.insert(new DummyTextureSource(2, 10), 10, 10, SPACING_NONE);
+		final Node inserted = node.insert(new DummyTextureSource(2, 10), 10, 10, SPACING_NONE, PADDING_NONE);
 
 		Assert.assertNull(inserted);
 	}
@@ -126,40 +128,47 @@ public class BlackPawnTextureBuilderTest extends TestCase {
 	public void testFullSize() {
 		final Node node = new Node(0, 0, 10, 10);
 
-		final Node inserted = node.insert(new DummyTextureSource(10, 10), 10, 10, SPACING_NONE);
+		final Node inserted = node.insert(new DummyTextureSource(10, 10), 10, 10, SPACING_NONE, PADDING_NONE);
 
 		this.assertNodeRect(inserted, 0, 0, 10, 10);
 	}
 
 	public void testPerfectFit() {
 		final Node node = new Node(0, 0, 10, 10);
-		node.insert(new DummyTextureSource(5, 5), 10, 10, SPACING_NONE);
-		node.insert(new DummyTextureSource(5, 5), 10, 10, SPACING_NONE);
-		node.insert(new DummyTextureSource(5, 5), 10, 10, SPACING_NONE);
+		node.insert(new DummyTextureSource(5, 5), 10, 10, SPACING_NONE, PADDING_NONE);
+		node.insert(new DummyTextureSource(5, 5), 10, 10, SPACING_NONE, PADDING_NONE);
+		node.insert(new DummyTextureSource(5, 5), 10, 10, SPACING_NONE, PADDING_NONE);
 
-		final Node inserted = node.insert(new DummyTextureSource(5, 5), 10, 10, SPACING_NONE);
+		final Node inserted = node.insert(new DummyTextureSource(5, 5), 10, 10, SPACING_NONE, PADDING_NONE);
 
 		this.assertNodeRect(inserted, 5, 5, 5, 5);
 	}
 
 	public void testPerfectFit2() {
 		final Node node = new Node(0, 0, 10, 10);
-		node.insert(new DummyTextureSource(4, 4), 10, 10, SPACING_ONE);
-		node.insert(new DummyTextureSource(4, 4), 10, 10, SPACING_ONE);
-		node.insert(new DummyTextureSource(4, 4), 10, 10, SPACING_ONE);
+		node.insert(new DummyTextureSource(4, 4), 10, 10, SPACING_ONE, PADDING_NONE);
+		node.insert(new DummyTextureSource(4, 4), 10, 10, SPACING_ONE, PADDING_NONE);
+		node.insert(new DummyTextureSource(4, 4), 10, 10, SPACING_ONE, PADDING_NONE);
 
-		final Node inserted = node.insert(new DummyTextureSource(5, 5), 10, 10, SPACING_ONE);
+		final Node inserted = node.insert(new DummyTextureSource(5, 5), 10, 10, SPACING_ONE, PADDING_NONE);
 
 		this.assertNodeRect(inserted, 5, 5, 5, 5);
 	}
 
 	public void testNoFitDueToSpacing() {
 		final Node node = new Node(0, 0, 10, 10);
-		node.insert(new DummyTextureSource(5, 5), 10, 10, SPACING_ONE);
+		node.insert(new DummyTextureSource(5, 5), 10, 10, SPACING_ONE, PADDING_NONE);
 
-		final Node inserted = node.insert(new DummyTextureSource(5, 5), 10, 10, SPACING_ONE);
+		final Node inserted = node.insert(new DummyTextureSource(5, 5), 10, 10, SPACING_ONE, PADDING_NONE);
 
 		Assert.assertNull(inserted);
+	}
+
+	public void testSinglePadding() {
+		final Node node = new Node(0, 0, 10, 10);
+		final Node inserted = node.insert(new DummyTextureSource(8, 8), 10, 10, SPACING_NONE, PADDING_ONE);
+
+		this.assertNodeRect(inserted, 0, 0, 10, 10);
 	}
 
 	private void assertNodeRect(final Node pNode, final int pLeft, final int pTop, final int pWidth, final int pHeight) {
