c3408c0b96950879f3891dd10f9d47a299b8dc19
Nicolas Gramlich <ngramlich@zynga.com>
2012-04-18 11:59:02 -0700

    Updated LineTest (Renamed from LineCollisionCheckerTest) to AndEngine->AnchorCenter branch changes.

--- src/org/andengine/entity/primitive/LineTest.java
+++ src/org/andengine/entity/primitive/LineTest.java
@@ -1,4 +1,4 @@
-package org.andengine.util.algorithm.collision;
+package org.andengine.entity.primitive;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
@@ -6,12 +6,13 @@ import junit.framework.TestCase;
 import org.andengine.entity.primitive.Line;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 import org.andengine.util.Constants;
+import org.andengine.util.algorithm.collision.LineCollisionChecker;
 
 /**
  * @author Nicolas Gramlich
  * @since 13:50:55 - 22.06.2011
  */
-public class LineCollisionCheckerTest extends TestCase {
+public class LineTest extends TestCase {
 	// ===========================================================
 	// Constants
 	// ===========================================================
@@ -54,32 +55,45 @@ public class LineCollisionCheckerTest extends TestCase {
 	// ===========================================================
 
 	public void testSimple() {
-		final Line line = new Line(-10, 0, 10, 0, this.mVertexBufferObjectManager);
+		final Line line = new Line(-1, 0, 1, 0, this.mVertexBufferObjectManager);
 
-		this.assertLine(-10, 0, 10, 0, line);
+		this.assertLine(-1, 0, 1, 0, line);
 	}
-	
+
+	public void testReverse() {
+		final Line line = new Line(1, 0, -1, 0, this.mVertexBufferObjectManager);
+
+		this.assertLine(1, 0, -1, 0, line);
+	}
+
 	public void testRotation90() {
-		final Line line = new Line(-10, 0, 10, 0, this.mVertexBufferObjectManager);
+		final Line line = new Line(-1, 0, 1, 0, this.mVertexBufferObjectManager);
 		line.setRotation(90);
 
-		this.assertLine(0, -10, 0, 10, line);
+		this.assertLine(0, -1, 0, 1, line);
 	}
-	
+
 	public void testRotation180() {
-		final Line line = new Line(-10, 0, 10, 0, this.mVertexBufferObjectManager);
+		final Line line = new Line(-1, 0, 1, 0, this.mVertexBufferObjectManager);
 		line.setRotation(180);
 
-		this.assertLine(10, 0, -10, 0, line);
+		this.assertLine(1, 0, -1, 0, line);
+	}
+
+	public void testReverseRotation180() {
+		final Line line = new Line(1, 0, -1, 0, this.mVertexBufferObjectManager);
+		line.setRotation(180);
+
+		this.assertLine(-1, 0, 1, 0, line);
 	}
 
 	private void assertLine(final int pX1, final int pY1, final int pX2, final int pY2, final Line pLine) {
-		LineCollisionChecker.fillVertices(pLine, VERTICES);
-		
-		Assert.assertEquals(pX1, VERTICES[0 + Constants.VERTEX_INDEX_X], DELTA);
-		Assert.assertEquals(pY1, VERTICES[0 + Constants.VERTEX_INDEX_Y], DELTA);
-		Assert.assertEquals(pX2, VERTICES[2 + Constants.VERTEX_INDEX_X], DELTA);
-		Assert.assertEquals(pY2, VERTICES[2 + Constants.VERTEX_INDEX_Y], DELTA);
+		LineCollisionChecker.fillVertices(pLine, LineTest.VERTICES);
+
+		Assert.assertEquals(pX1, LineTest.VERTICES[0 + Constants.VERTEX_INDEX_X], LineTest.DELTA);
+		Assert.assertEquals(pY1, LineTest.VERTICES[0 + Constants.VERTEX_INDEX_Y], LineTest.DELTA);
+		Assert.assertEquals(pX2, LineTest.VERTICES[2 + Constants.VERTEX_INDEX_X], LineTest.DELTA);
+		Assert.assertEquals(pY2, LineTest.VERTICES[2 + Constants.VERTEX_INDEX_Y], LineTest.DELTA);
 	}
 
 	// ===========================================================
