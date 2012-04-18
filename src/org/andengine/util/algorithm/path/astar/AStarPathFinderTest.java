63809286375358e6e80db89babf21afe3146519d
Nicolas Gramlich <ngramlich@zynga.com>
2012-04-18 11:59:48 -0700

    Made it very very very easy to add test cases to AStarPathFinderTest.

--- src/org/andengine/util/algorithm/path/astar/AStarPathFinderTest.java
+++ src/org/andengine/util/algorithm/path/astar/AStarPathFinderTest.java
@@ -6,6 +6,7 @@ import junit.framework.TestCase;
 import org.andengine.util.algorithm.path.ICostFunction;
 import org.andengine.util.algorithm.path.IPathFinderMap;
 import org.andengine.util.algorithm.path.Path;
+import org.andengine.util.algorithm.path.astar.AStarPathFinder.Node;
 
 /**
  * @author Nicolas Gramlich
@@ -29,33 +30,132 @@ public class AStarPathFinderTest extends TestCase {
 	// ===========================================================
 
 	public void testSimplePath() throws Exception {
-		final AStarPathFinder<Object> aStarPathFinder = new AStarPathFinder<Object>();
-		
-		final Path path = aStarPathFinder.findPath(new IPathFinderMap<Object>() {
-			@Override
-			public boolean isBlocked(int pX, int pY, Object pEntity) {
-				return false;
-			}
-		},
-		0, 0, 2, 2,
-		new Object(),
-		0, 0, 2, 2,
-		false,
-		new EuclideanHeuristic<Object>(),
-		new ICostFunction<Object>() {
-			@Override
-			public float getCost(IPathFinderMap<Object> pPathFinderMap, int pFromX, int pFromY, int pToX, int pToY, Object pEntity) {
-				return 1;
-			}
+		final Path path = AStarPathFinderTest.getPath(new String[] {
+				"S  ",
+				"   ",
+				"  F",
 		});
 		Assert.assertEquals(5, path.getLength());
 	}
-	
+
+	public void testCalculateNodeID() {
+		this.testCalculateNodeID(0xFFFFFFFFFFFFFFFFL, 0xFFFFFFFF, 0xFFFFFFFF);
+		this.testCalculateNodeID(0x8000000080000000L, 0x80000000, 0x80000000);
+	}
+
 	// ===========================================================
 	// Methods
 	// ===========================================================
 
+	private static Path getPath(final String[] pMap) {
+		return AStarPathFinderTest.getPath(pMap, false);
+	}
+
+	private static Path getPath(final String[] pMap, final boolean pAllowDiagonal) {
+		return AStarPathFinderTest.getPath(pMap, pAllowDiagonal,
+				new EuclideanHeuristic<Object>(),
+				new ICostFunction<Object>() {
+			@Override
+			public float getCost(final IPathFinderMap<Object> pPathFinderMap, final int pFromX, final int pFromY, final int pToX, final int pToY, final Object pEntity) {
+				return 1;
+			}
+		});
+	}
+
+	private static Path getPath(final String[] pMap, final boolean pAllowDiagonal, final IAStarHeuristic<Object> pAStarHeuristic, final ICostFunction<Object> pCostFunction) {
+		final int fromX = AStarPathFinderTest.findColumnOfFirstOccurrence(pMap, 'S');
+		final int fromY = AStarPathFinderTest.findRowOfFirstOccurrence(pMap, 'S');
+		final int toX = AStarPathFinderTest.findColumnOfFirstOccurrence(pMap, 'F');
+		final int toY = AStarPathFinderTest.findRowOfFirstOccurrence(pMap, 'F');
+
+		final int minX = 0;
+		final int minY = 0;
+		final int maxX = pMap[0].length() - 1;
+		final int maxY = pMap.length - 1;
+
+		final Path path = new AStarPathFinder<Object>().findPath(new SimplePathFinderMap(pMap),
+				minX, minY, maxX, maxY,
+				new Object(),
+				fromX, fromY, toX, toY,
+				pAllowDiagonal,
+				pAStarHeuristic,
+				pCostFunction);
+		return path;
+	}
+
+	private static int findRowOfFirstOccurrence(final String[] pMap, final char pCharacter) {
+		for(int y = 0; y < pMap.length; y++) {
+			final char[] row = pMap[y].toCharArray();
+			for(int x = 0; x < row.length; x++) {
+				if(row[x] == pCharacter) {
+					return y;
+				}
+			}
+		}
+		throw new IllegalArgumentException();
+	}
+
+	private static int findColumnOfFirstOccurrence(final String[] pMap, final char pCharacter) {
+		for(int y = 0; y < pMap.length; y++) {
+			final char[] row = pMap[y].toCharArray();
+			for(int x = 0; x < row.length; x++) {
+				if(row[x] == pCharacter) {
+					return x;
+				}
+			}
+		}
+		throw new IllegalArgumentException();
+	}
+
+	private void testCalculateNodeID(final long pExpected, final int pX, final int pY) {
+		final long actual = Node.calculateID(pX, pY);
+		if(pExpected != actual) {
+			Assert.fail("Expected: " + Long.toBinaryString(pExpected) + " Actual: " + Long.toBinaryString(actual));
+		}
+	}
+
 	// ===========================================================
 	// Inner and Anonymous Classes
 	// ===========================================================
+
+	private static class SimplePathFinderMap implements IPathFinderMap<Object> {
+		// ===========================================================
+		// Constants
+		// ===========================================================
+
+		// ===========================================================
+		// Fields
+		// ===========================================================
+
+		private final String[] mMap;
+
+		// ===========================================================
+		// Constructors
+		// ===========================================================
+
+		public SimplePathFinderMap(final String[] pMap) {
+			this.mMap = pMap;
+		}
+
+		// ===========================================================
+		// Getter & Setter
+		// ===========================================================
+
+		// ===========================================================
+		// Methods for/from SuperClass/Interfaces
+		// ===========================================================
+
+		@Override
+		public boolean isBlocked(final int pX, final int pY, final Object pEntity) {
+			return this.mMap[pY].charAt(pX) == '#';
+		}
+
+		// ===========================================================
+		// Methods
+		// ===========================================================
+
+		// ===========================================================
+		// Inner and Anonymous Classes
+		// ===========================================================
+	}
 }
