dab8ffb349e681e19e2ceaa9b24483e7cd1f23f3
Nicolas Gramlich <ngramlich@zynga.com>
2013-05-18 19:49:38 -0700

    Renamed PriorityAggregatorQueueTest to PriorityBlockingAggregatorQueueTest.

--- src/org/andengine/util/adt/list/PriorityBlockingAggregatorQueueTest.java
+++ src/org/andengine/util/adt/list/PriorityBlockingAggregatorQueueTest.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 
 import junit.framework.Assert;
 
+import org.andengine.util.adt.queue.concurrent.PriorityBlockingAggregatorQueue;
 import org.junit.Test;
 
 import android.test.AndroidTestCase;
@@ -13,7 +14,7 @@ import android.test.AndroidTestCase;
  * @author Nicolas Gramlich
  * @since 22:31:38 - 16.09.2010
  */
-public class PriorityAggregatorQueueTest extends AndroidTestCase {
+public class PriorityBlockingAggregatorQueueTest extends AndroidTestCase {
 	// ===========================================================
 	// Constants
 	// ===========================================================
@@ -27,16 +28,16 @@ public class PriorityAggregatorQueueTest extends AndroidTestCase {
 	private static final int CAPACITY_ONE = 1;
 
 	private static final int PRIORITY_HIGHEST = 0;
-	private static final int PRIORITY_HIGH = PriorityAggregatorQueueTest.PRIORITY_HIGHEST + 1;
-	private static final int PRIORITY_MEDIUM = PriorityAggregatorQueueTest.PRIORITY_HIGH + 1;
-	private static final int PRIORITY_LOW = PriorityAggregatorQueueTest.PRIORITY_MEDIUM + 1;
-	private static final int PRIORITY_LOWEST = PriorityAggregatorQueueTest.PRIORITY_LOW + 1;
+	private static final int PRIORITY_HIGH = PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST + 1;
+	private static final int PRIORITY_MEDIUM = PriorityBlockingAggregatorQueueTest.PRIORITY_HIGH + 1;
+	private static final int PRIORITY_LOW = PriorityBlockingAggregatorQueueTest.PRIORITY_MEDIUM + 1;
+	private static final int PRIORITY_LOWEST = PriorityBlockingAggregatorQueueTest.PRIORITY_LOW + 1;
 
 	// ===========================================================
 	// Fields
 	// ===========================================================
 
-	private PriorityAggregatorQueue<String> mPriorityAggregatorQueue;
+	private PriorityBlockingAggregatorQueue<String> mPriorityAggregatorQueue;
 
 	// ===========================================================
 	// Constructors
@@ -44,7 +45,7 @@ public class PriorityAggregatorQueueTest extends AndroidTestCase {
 
 	@Override
 	protected void setUp() throws Exception {
-		this.mPriorityAggregatorQueue = new PriorityAggregatorQueue<String>(true);
+		this.mPriorityAggregatorQueue = new PriorityBlockingAggregatorQueue<String>(true);
 	}
 
 	@Override
@@ -61,16 +62,16 @@ public class PriorityAggregatorQueueTest extends AndroidTestCase {
 	// ===========================================================
 
 	public void testPut() throws InterruptedException {
-		this.mPriorityAggregatorQueue.addQueue(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
+		this.mPriorityAggregatorQueue.addQueue(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
 
-		this.mPriorityAggregatorQueue.put(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, "A");
+		this.mPriorityAggregatorQueue.put(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, "A");
 
 		Assert.assertEquals(1, this.mPriorityAggregatorQueue.size());
 	}
 
 	public void testPutIsBlocking() throws InterruptedException {
-		final PriorityAggregatorQueue<String> paq = PriorityAggregatorQueueTest.this.mPriorityAggregatorQueue;
-		this.mPriorityAggregatorQueue.addQueue(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
+		final PriorityBlockingAggregatorQueue<String> paq = PriorityBlockingAggregatorQueueTest.this.mPriorityAggregatorQueue;
+		this.mPriorityAggregatorQueue.addQueue(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
 
 		final CountDownLatch timeoutCountDownLatch = new CountDownLatch(1);
 		final CountDownLatch putCountDownLatch = new CountDownLatch(1);
@@ -80,9 +81,9 @@ public class PriorityAggregatorQueueTest extends AndroidTestCase {
 			public void runFallible() throws InterruptedException {
 				putCountDownLatch.await();
 
-				paq.put(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, "A");
+				paq.put(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, "A");
 
-				paq.put(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, "B");
+				paq.put(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, "B");
 
 				timeoutCountDownLatch.countDown();
 			}
@@ -94,7 +95,7 @@ public class PriorityAggregatorQueueTest extends AndroidTestCase {
 	}
 
 	public void testPutAggregated() throws InterruptedException {
-		final PriorityAggregatorQueue<String> paq = PriorityAggregatorQueueTest.this.mPriorityAggregatorQueue;
+		final PriorityBlockingAggregatorQueue<String> paq = PriorityBlockingAggregatorQueueTest.this.mPriorityAggregatorQueue;
 
 		final CountDownLatch startCountDownLatch = new CountDownLatch(CAPACITY_HIGHEST);
 		final CountDownLatch putCountDownLatch = new CountDownLatch(1);
@@ -134,34 +135,34 @@ public class PriorityAggregatorQueueTest extends AndroidTestCase {
 	}
 
 	public void testOffer() throws InterruptedException {
-		this.mPriorityAggregatorQueue.addQueue(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
+		this.mPriorityAggregatorQueue.addQueue(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
 
-		final boolean success = this.mPriorityAggregatorQueue.offer(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, "A");
+		final boolean success = this.mPriorityAggregatorQueue.offer(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, "A");
 		Assert.assertTrue(success);
 
 		Assert.assertEquals(1, this.mPriorityAggregatorQueue.size());
 	}
 
 	public void testOfferOnFull() throws InterruptedException {
-		this.mPriorityAggregatorQueue.addQueue(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
+		this.mPriorityAggregatorQueue.addQueue(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
 
-		this.mPriorityAggregatorQueue.offer(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, "A");
-		final boolean success = this.mPriorityAggregatorQueue.offer(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, "B");
+		this.mPriorityAggregatorQueue.offer(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, "A");
+		final boolean success = this.mPriorityAggregatorQueue.offer(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, "B");
 
 		Assert.assertFalse(success);
 	}
 
 	public void testPeek() throws InterruptedException {
-		this.mPriorityAggregatorQueue.addQueue(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
+		this.mPriorityAggregatorQueue.addQueue(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
 
-		this.mPriorityAggregatorQueue.put(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, "A");
+		this.mPriorityAggregatorQueue.put(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, "A");
 		Assert.assertEquals("A", this.mPriorityAggregatorQueue.peek());
 
 		Assert.assertEquals(1, this.mPriorityAggregatorQueue.size());
 	}
 
 	public void testPeekOnEmpty() throws InterruptedException {
-		this.mPriorityAggregatorQueue.addQueue(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
+		this.mPriorityAggregatorQueue.addQueue(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
 
 		Assert.assertNull(this.mPriorityAggregatorQueue.peek());
 
@@ -169,16 +170,16 @@ public class PriorityAggregatorQueueTest extends AndroidTestCase {
 	}
 
 	public void testPoll() throws InterruptedException {
-		this.mPriorityAggregatorQueue.addQueue(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
+		this.mPriorityAggregatorQueue.addQueue(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
 
-		this.mPriorityAggregatorQueue.put(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, "A");
+		this.mPriorityAggregatorQueue.put(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, "A");
 		Assert.assertEquals("A", this.mPriorityAggregatorQueue.poll());
 
 		Assert.assertEquals(0, this.mPriorityAggregatorQueue.size());
 	}
 
 	public void testPollOnEmpty() throws InterruptedException {
-		this.mPriorityAggregatorQueue.addQueue(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
+		this.mPriorityAggregatorQueue.addQueue(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
 
 		Assert.assertNull(this.mPriorityAggregatorQueue.poll());
 
@@ -186,17 +187,17 @@ public class PriorityAggregatorQueueTest extends AndroidTestCase {
 	}
 
 	public void testTake() throws InterruptedException {
-		this.mPriorityAggregatorQueue.addQueue(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
+		this.mPriorityAggregatorQueue.addQueue(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
 
-		this.mPriorityAggregatorQueue.put(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, "A");
+		this.mPriorityAggregatorQueue.put(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, "A");
 		Assert.assertEquals("A", this.mPriorityAggregatorQueue.take());
 
 		Assert.assertEquals(0, this.mPriorityAggregatorQueue.size());
 	}
 
 	public void testTakeIsBlockingOnEmpty() throws InterruptedException {
-		final PriorityAggregatorQueue<String> paq = PriorityAggregatorQueueTest.this.mPriorityAggregatorQueue;
-		this.mPriorityAggregatorQueue.addQueue(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
+		final PriorityBlockingAggregatorQueue<String> paq = PriorityBlockingAggregatorQueueTest.this.mPriorityAggregatorQueue;
+		this.mPriorityAggregatorQueue.addQueue(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_ONE);
 
 		final CountDownLatch timeoutCountDownLatch = new CountDownLatch(1);
 		final CountDownLatch pollCountDownLatch = new CountDownLatch(1);
@@ -218,8 +219,8 @@ public class PriorityAggregatorQueueTest extends AndroidTestCase {
 	}
 
 	public void testPutWithTake() throws InterruptedException {
-		final PriorityAggregatorQueue<String> paq = PriorityAggregatorQueueTest.this.mPriorityAggregatorQueue;
-		this.mPriorityAggregatorQueue.addQueue(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_TWO);
+		final PriorityBlockingAggregatorQueue<String> paq = PriorityBlockingAggregatorQueueTest.this.mPriorityAggregatorQueue;
+		this.mPriorityAggregatorQueue.addQueue(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, CAPACITY_TWO);
 
 		final CountDownLatch takeCountDownLatch = new CountDownLatch(1);
 		final CountDownLatch putCountDownLatch = new CountDownLatch(1);
@@ -230,11 +231,11 @@ public class PriorityAggregatorQueueTest extends AndroidTestCase {
 			public void runFallible() throws InterruptedException {
 				putCountDownLatch.await();
 
-				paq.put(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, "A");
+				paq.put(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, "A");
 
 				takeCountDownLatch.countDown();
 
-				paq.put(PriorityAggregatorQueueTest.PRIORITY_HIGHEST, "B");
+				paq.put(PriorityBlockingAggregatorQueueTest.PRIORITY_HIGHEST, "B");
 
 				completeCountDownLatch.countDown();
 			}
